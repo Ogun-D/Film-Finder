3a45308232f49ed107ffe3dc3616ffdfec6726e7
Demi Ogun <demiogun1@gmail.com>
2019-09-17 23:41:03 +0100

    Lifted state up to App component

--- src/App.js
+++ src/App.js
@@ -4,11 +4,23 @@ import './css/App.css';
 import SearchBar from './components/SearchBar';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {search: ''};
+  }
+
+  handleChange = (event) => {
+    this.setState({search: event});
+  }
 
   render() {
+    const search = this.state.search;
     return (
       <div className="app-container">
-        <SearchBar value="Goodbye" />
+        <SearchBar
+          search={search}
+          onValueChange={this.handleChange}
+        />
 
       </div>
     );
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,13 +2,34 @@ import React from 'react';
 import '../css/SearchBar.css'
 
 class SearchBar extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.handleChange = this.handleChange.bind(this);
+  // }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    const search = document.querySelector('.song-search');
+    console.log(search.value);
+  }
+
+  handleChange = (event) => {
+    this.props.onValueChange(event.target.value);
+  }
 
   render() {
+    const search = this.props.search;
     return(
       <div className="search-bar">
 
-        <form className="song-selector" onSubmit={} >
-          <input type="text" placeholder="Search for a Song" className="song-search" />
+        <form className="song-selector" onSubmit={this.handleSubmit} >
+          <input
+            type="text"
+            placeholder="Search for a Song"
+            className="song-search"
+            value={search}
+            onChange={this.handleChange} />
           <button type="submit">Find this Song</button>
         </form>
 
