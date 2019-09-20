7049199caaab1ce526f3eae285174bce214c5621
Demi Ogun <demiogun1@gmail.com>
2019-09-20 15:03:50 +0100

    Updates

---
+++ precache-manifest.c50d096091692ce54141140ebe168dac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef206dba092316ee5b20dc79781a27b5",
+    "url": "/film-finder/index.html"
+  },
+  {
+    "revision": "877f3c20cb503d247ca6",
+    "url": "/film-finder/static/css/main.8e4c9342.chunk.css"
+  },
+  {
+    "revision": "74e3e4b97c29747e1288",
+    "url": "/film-finder/static/js/2.f66d9c97.chunk.js"
+  },
+  {
+    "revision": "877f3c20cb503d247ca6",
+    "url": "/film-finder/static/js/main.a8bce7f6.chunk.js"
+  },
+  {
+    "revision": "11c4f79fdfd7e4d27939",
+    "url": "/film-finder/static/js/runtime~main.92184784.js"
+  }
+]);
